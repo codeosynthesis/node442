2ccb9e2f74494477ceb1b5cc10cb0ca9c8b148f3
jjw9128 <jjw9128@vm-0.jjw9128.koding.kd.io>
2014-04-17 08:26:29 +0000

    first 3 tuts, installed nodemon, got express running

--- express/abtut/app.js
+++ express/abtut/app.js
@@ -1,36 +1,35 @@
-
+#!/usr/bin/node
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes  = require('./routes');
 
 var app = module.exports = express.createServer();
 
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.get('/',function(req,res)
+{
+    res.send('hellow express');
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.get('/hi',function(req,res,next)
+{	
+    var message = [
+        '<h1> hello, express </h1>',
+        '<p>welcom to buildig web apps in nodejs with express </p>',
+        '<p> youll love express because it\'s </p>',
+        '<ul><li>fast</li><li>fun</li><li>flexable</li></ul>',].join('\n');
+        res.send(message);
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
 
-// Routes
 
-app.get('/', routes.index);
+console.log('hi'+app.get('port'));
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+app.listen(3000);
+
+// dont work
+//http.createServer(app).listen(app.get('port'), function(){
+//	console.log('express server listening on port ' + app.get('port'));
+//}); 
---
+++ express/abtut/tests.sh
@@ -0,0 +1,5 @@
+node --version
+npm --version
+express --version
+
+
